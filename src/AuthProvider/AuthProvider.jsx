bde2b66bea7373cf9dc568aa4f0c594a004124fa
afnanhussain2002 <afnanhussain2022@gmail.com>
2023-10-03 23:40:29 +0600

    adding sign in or sign out logic

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -1,10 +1,12 @@
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
-import { createContext } from "react";
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
 import { auth } from "../firebase/firebase.config";
 import PropTypes from 'prop-types'; 
 
 export const AuthContext = createContext(null)
 const AuthProvider = ({children}) => {
+
+    const [user, setUser] = useState([])
 
     const createUser = (email, password) =>{
        return createUserWithEmailAndPassword(auth, email, password)
@@ -14,7 +16,22 @@ const AuthProvider = ({children}) => {
        return signInWithEmailAndPassword(auth, email, password)
     }
 
-    const authInfo = {createUser, loginUser}
+    const logOut = () =>{
+       return signOut(auth)
+    }
+
+    useEffect(()=>{
+        const unSubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            console.log('useeffect with observe current user', currentUser)
+
+        })
+        return ()=>{
+            unSubscribe();
+        }
+    },[])
+
+    const authInfo = {user, createUser, loginUser, logOut}
     return (
         <>
             <AuthContext.Provider value={authInfo}>
